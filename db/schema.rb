6d8b2493cfd26891f61348acb0f0243d83a390ad
1codechic <1codechic@devchics.com>
2019-04-24 15:16:32 -0400

    added bcrypt gem, updated users schema and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_221602) do
+ActiveRecord::Schema.define(version: 2019_04_24_191458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,9 +41,10 @@ ActiveRecord::Schema.define(version: 2019_04_23_221602) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "name"
   end
 
 end
